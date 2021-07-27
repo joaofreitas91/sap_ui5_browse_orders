d76f6278091ca7897f4ef7a7a346cdd9779c0680
João <jp.contatoprofissional@gmail.com>
2021-07-27 12:13:35 -0300

    Teste com expand

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,21 +1,26 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-], function (Controller) {
+    'sap/ui/model/odata/v2/ODataModel',
+    'sap/ui/model/json/JSONModel'
+], function (Controller, ODataModel, JSONModel) {
     'use strict';
 
     return Controller.extend("ProjBrowseOrders.browseorders.controller.Main", {
         onInit: function () {
-            const service = "/Northwind/V2/(S(ktypfzlwprqyezyjst0nncbc))/OData/OData.svc/"
-            const person = {
-                headers: { "Content-ID": "0" }
-            }
-            this.odata = new sap.ui.model.odata.v2.ODataModel(service, person)
+            const service = "/Northwind/V2/Northwind/Northwind.svc/"
+            // const person = {
+            //     headers: { "Content-ID": "0" }
+            // }
+            this.odata = new ODataModel(service)
 
-            this.odata.read("/Products", {
+            this.odata.read("/Orders", {
+                urlParameters: {
+                    "$expand": "Customer"
+                },
                 success: function (response) {
-                    console.log(response.results)
+                    console.log(response.results[0])
 
-                    const model = new sap.ui.model.json.JSONModel(response.results)
+                    const model = new JSONModel(response.results)
                     this.getView().setModel(model, "myModel")
                 }.bind(this),
 
