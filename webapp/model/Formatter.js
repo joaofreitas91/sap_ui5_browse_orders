7541758d002c4e124d9a6d317e69c046da91469a
João <jp.contatoprofissional@gmail.com>
2021-07-28 19:35:12 -0300

    Implementação do Texto e Estado das Entragas

--- webapp/model/Formatter.js
+++ webapp/model/Formatter.js
@@ -3,7 +3,6 @@ sap.ui.define([], function () {
     return {
         dateFormatMonths: function (date) {
             const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-            var dateFormatated = (date.getFullYear() + "/" + ((date.getMonth() + 1)) + "/" + (date.getDate()));
             var dateFormatated = (`${date.getDate()} de ${months[(date.getMonth())]} de ${(date.getFullYear())} `);
             return dateFormatated
         },
@@ -13,10 +12,41 @@ sap.ui.define([], function () {
                     return "0" + number
                 else
                     return number
-
             }
+
             var dateFormatated = (addZero(date.getDate()) + "/" + addZero(((date.getMonth() + 1))) + "/" + (date.getFullYear()));
             return dateFormatated
+        },
+        shippedText: function (requiredDate, shippedDate, shippedOrder) {
+
+            const betweenDays = requiredDate - shippedDate
+            const difference = betweenDays / 1000 / 60 / 60 / 24
+
+            // console.log(`A ordem ${shippedOrder} foi entregue com ${difference} dias de antecendencia`)
+
+            if (difference < 0) {
+                return "Too Late"
+            } else if (difference >= 0 && difference < 20) {
+                return "Urgent"
+            } else {
+                return "In Time"
+            }
+
+        },
+        shippedState: function (requiredDate, shippedDate) {
+
+            const betweenDays = requiredDate - shippedDate
+            const difference = betweenDays / 1000 / 60 / 60 / 24
+
+            // console.log(`A ordem ${shippedOrder} foi entregue com ${difference} dias de antecendencia`)
+
+            if (difference < 0) {
+                return "Error"
+            } else if (difference >= 0 && difference < 20) {
+                return "Warning"
+            } else {
+                return "Success"
+            }
         }
     }
 });
